cd7dbd4b0842029f78b93164c018fb4a14f22de6
YoonJiwon <gaden119000@gmail.com>
2025-10-28 22:59:26 +0900

    first commit

---
+++ src/main/java/com/packii/PackiiApplication.java
@@ -0,0 +1,13 @@
+package com.packii;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PackiiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PackiiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/packii/PackiiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.packii;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PackiiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
