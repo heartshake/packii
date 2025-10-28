82f01804a48e1b8f0503dfd4bcec5fafe84a3e39
YoonJiwon <gaden119000@gmail.com>
2025-10-28 23:05:28 +0900

    public 제거

--- src/main/java/com/packii/PackiiApplication.java
+++ src/main/java/com/packii/PackiiApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PackiiApplication {
+class PackiiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PackiiApplication.class, args);
