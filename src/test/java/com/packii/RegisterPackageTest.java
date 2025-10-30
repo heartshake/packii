965ff6c3f07fd22f0fff8fa584b5d6863108a8d5
ori0o0p <tmddnjsrla99@gmail.com>
2025-10-30 21:53:14 +0900

    실패하는 테스트 작성

--- src/test/java/com/packii/RegisterPackageTest.java
+++ src/test/java/com/packii/RegisterPackageTest.java
@@ -9,7 +9,10 @@ public class RegisterPackageTest {
     @DisplayName("택배를 등록한다.")
     void register_package() {
         // 학생 정보(이름)를 입력한다.
+        String studentName = "김승원";
         // 택배사 이름(예: CJ, 롯데, 한진, 우체국)을 입력한다.
+        String deliveryCompany = "CJ 대한통운";
         // 택배를 저장한다.
+        registerPackage.register(studentName, deliveryCompany);
     }
 }
