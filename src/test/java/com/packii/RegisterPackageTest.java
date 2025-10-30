be4928701f2c63d99f8402be482cce5ec6ba6e62
ori0o0p <tmddnjsrla99@gmail.com>
2025-10-30 21:49:57 +0900

    구현 계획 작성

--- src/test/java/com/packii/RegisterPackageTest.java
+++ src/test/java/com/packii/RegisterPackageTest.java
@@ -1,4 +1,15 @@
 package com.packii;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class RegisterPackageTest {
+
+    @Test
+    @DisplayName("택배를 등록한다.")
+    void register_package() {
+        // 학생 정보(이름)를 입력한다.
+        // 택배사 이름(예: CJ, 롯데, 한진, 우체국)을 입력한다.
+        // 택배를 저장한다.
+    }
 }
