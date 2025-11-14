34e02237af4b208967404f55d8f99068a54a0b8a
ori0o0p <tmddnjsrla99@gmail.com>
2025-11-14 22:02:42 +0900

    .

---
+++ src/main/java/com/packii/Delivery.java
@@ -0,0 +1,11 @@
+package com.packii;
+
+class Delivery {
+    String studentName;
+    String deliveryCompany;
+
+    public Delivery(String studentName, String deliveryCompany) {
+        this.studentName = studentName;
+        this.deliveryCompany = deliveryCompany;
+    }
+}
