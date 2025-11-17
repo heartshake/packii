843924ef586d6b8a33748a7c5f9527097a28613b
ori0o0p <tmddnjsrla99@gmail.com>
2025-11-17 22:27:38 +0900

    .

--- src/main/java/com/packii/Delivery.java
+++ src/main/java/com/packii/Delivery.java
@@ -1,10 +1,16 @@
 package com.packii;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 class Delivery {
+    private static final AtomicInteger counter = new AtomicInteger(0);
+
+    int id;
     String studentName;
     String deliveryCompany;
 
     public Delivery(String studentName, String deliveryCompany) {
+        this.id = counter.incrementAndGet();
         this.studentName = studentName;
         this.deliveryCompany = deliveryCompany;
     }
